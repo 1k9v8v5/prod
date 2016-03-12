33cd37a791d5b891736cdc9daa9e4732c7eca415
1k9v8v5 <1k9v8v5@mail.ru>
2016-03-12 18:24:13 +0400

    5

--- app/src/main/java/com/bask/prod/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.bask.prod;
-
-public interface Constants
-{
-	public static final String DB_NAME = "prod.db";
-	public static final String DB_TABLE = "product";
-	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_NAME = "name";
-}
